4e340ac69d324419ca0ea4414968163f248e802f
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-06-10 21:03:20 -0300

    update connection

--- api/config/dbConnection.js
+++ api/config/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose') 
 
 const connectDb = () =>{
-    mongoose.connect(process.env.DB_URI)
+    mongoose.connect(process.env.DB_URI, {useNewUrlparser: true, useUnifiedTopology: true})
     .then(data => {
         console.log(`Conectado com o servidor: ${data.connection.host}`)
     }) 
